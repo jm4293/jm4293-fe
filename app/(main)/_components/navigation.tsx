75fb7b1623638e8a73e7f70172013e6f71dacfc5
jm4293 <dlwoals4293@gmail.com>
2024-11-07 11:33:43 +0900

    [Design]: 하단 네비게이션바 데스크탑에서도 중앙에 오도록 수정

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <nav className="fixed bottom-0 w-full bg-white min-h-[60px] max-h-[60px] flex justify-between items-center pr-6 pl-6">
+    <nav className="fixed bottom-0 left-[50%] translate-x-[-50%] w-[100vw] min-w-[320px] max-w-[1024px] bg-white min-h-[60px] max-h-[60px] flex justify-between items-center pr-6 pl-6">
       <Link href="/board">Board</Link>
       <Link href="/chatting">Chatting</Link>
       <Link href="/my">My</Link>
